54beb7171317b762f039ec6a6124a2e06a1a6ccf
krishnakaveri <krishnaaky142000@gmail.com>
2022-11-02 21:31:24 +0530

    prime and palindrome

---
+++ prime.js
@@ -0,0 +1,24 @@
+function prime(number){
+
+   let factor=0;
+
+   for(i=1;i<=number;i++){
+
+      if(num % i == 0){
+
+         factor++;
+      }
+}
+      if(factor == 2){
+         return true
+     } 
+       return false
+    }
+}
+let ans=prime(5);
+
+if(ans==true){
+     console.log(number,"prime number")
+}else{
+     console.log(number,"is a not prime number")
+}
